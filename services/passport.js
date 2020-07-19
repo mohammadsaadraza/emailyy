5074d739ed3151410426fdb25f6756e07121c055
Saad Raza <saad.dr98@gmail.com>
2020-07-20 00:16:06 +0500

    google strategy tweak

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,7 @@ passport.use(
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
 			callbackURL: "/auth/google/callback",
+			proxy: true,
 		},
 		async (accessToken, refreshToken, profile, done) => {
 			try {
